af331419d5f9fe9db350914c8830399eda0d4047
Cheng Ni <cn1036@nyu.edu>
2017-02-04 18:21:34 -0500

    add predict_richness.sh

--- predict_richness.sh
+++ predict_richness.sh
@@ -1,1 +1,2 @@
-cat areas1.txt areas2.txt areas3.txt areas4.txt areas5.txt areas6.txt | python rich_pred.py | sort -t ',' -g
+cat areas1.txt areas2.txt areas3.txt areas4.txt areas5.txt areas6.txt | python rich_pred.py | sort -t ',' -g -u
+
